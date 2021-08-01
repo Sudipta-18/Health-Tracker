05c58c81f81036fcdf0c5280ee852e690f5cb4af
Sudipta Singhal <sudipta482000@gmail.com>
2021-08-01 17:24:23 +0530

    Water Tracker webpage

---
+++ water-track/app.js
@@ -0,0 +1,61 @@
+// Button Selectors
+const addButton = document.querySelector(".add"),
+    removeButton = document.querySelector(".remove");
+
+// Elements for update
+const currentCupsEl = document.querySelector('.current-cups'),
+    currentLitersEl = document.querySelector('.current-liters'),
+    currentPercentageEl = document.querySelector('.current-percentage'),
+    progressArea = document.querySelector(".progress")
+
+
+const MAX_CUPS = 10,
+    MIN_CUPS = 0;
+
+let cups = 0,
+    liters = 0,
+    percentage = 0;
+
+addButton.addEventListener("click", addCup);
+removeButton.addEventListener("click", removeCup);
+
+function addCup() {
+    cups++;
+    liters += 250;
+    percentage = (cups / MAX_CUPS) * 100;
+
+    // update layout
+    updateLayout();
+
+    if (cups === MAX_CUPS) {
+        addButton.disabled = true;
+    }
+    else {
+        removeButton.disabled = false;
+    }
+
+}
+
+function removeCup() {
+    cups--;
+    liters -= 250;
+    percentage = (cups / MAX_CUPS) * 100;
+
+    // update layout
+    updateLayout();
+
+    if (cups === MIN_CUPS) {
+        removeButton.disabled = true;
+    }
+    else {
+        addButton.disabled = false;
+    }
+
+}
+
+function updateLayout() {
+    currentCupsEl.textContent = `${cups}/10`;
+    currentLitersEl.textContent = `${liters / 1000}l/2.5l`;
+    currentPercentageEl.textContent = `${percentage}%`;
+    currentPercentageEl.style.height = `${percentage}%`;
+}
